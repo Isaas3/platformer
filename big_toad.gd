bf736c6cbd386dd03c89dccfd605dc398c00be13
Isaas3 <Isaas.Neely@searcyschools.org>
2023-10-24 11:16:25 -0500

    Began adding patrolling enemy

---
+++ big_toad.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+var SPEED = -25.0
+var facing_right = false
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var is_alive = true
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
+func _ready():
+	add_to_group("Enemy")
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	if !$RayCast2D.is_colliding() && is_on_floor():
+		flip() """9:25"""
+	# Set the horizontal velocity to a constant negative value to move left
+	velocity.x = -SPEED
+	
+	update_animation()
+	move_and_slide()
+	
+func update_animation():
+	animated_sprite_2d.play("hop")
+
+
+func _on_hitbox_body_entered(body):
+	if body.is_in_group("Player"):
+		body.velocity.y = -200
+		is_alive = false
+		queue_free()
